4f2e3aa6f6a24d932ac9d2b9fd33481522e08e2c
Martin Rist <mlr-github@martinrist.com>
2017-09-02 19:52:35 +0100

    Add logging.  Write results to page

--- examples/autocomplete.js
+++ examples/autocomplete.js
@@ -1,9 +1,10 @@
-/*global Rx*/
+/*global Rx, document*/
 
 // Search Wikipedia for a given term
 function searchWikipedia(term) {
+    console.log("Searching for:  " + term);
     return Rx.DOM.jsonpRequest({
-        url: "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiSearchCallback&search=" + term,
+        url: "http://en.wikipedia.org/w/api.php?action=opensearch&callback=wikiSearchCallback&search=" + term,
         jsonpCallback: "wikiSearchCallback"
     });
 }
@@ -15,18 +16,25 @@ function init() {
 
     // Get all distinct key up events from the input and only fire if long enough and distinct
     const keyup = Rx.Observable
-        .fromEvent(input, 'keyup')
+        .fromEvent(input, "keyup")
         .map(e => e.target.value)               // Project the text from the input
         .filter(text => text.length > 2)        // Only if the text is longer than 2 characters
         .debounce(750 /* Pause for 750ms */)
         .distinctUntilChanged(); // Only if the value has changed
 
-    keyup.subscribe(console.log);
     const searcher = keyup.flatMapLatest(searchWikipedia)
         .pluck("response")
         .map(r => r[1]);
 
-    searcher.subscribe(console.log);
+    searcher.subscribe(rs => {
+        console.log("Got results:", rs);
+        results.innerHTML = "";
+        rs.forEach(r => {
+            const item = document.createElement("li");
+            item.textContent = r;
+            results.appendChild(item);
+        });
+    });
 }
 
 Rx.DOM.ready().subscribe(init);
