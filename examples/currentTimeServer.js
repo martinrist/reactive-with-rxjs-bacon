9ee8cab543f37fb1a343a90c2b93966994a656d5
Martin Rist <mlr-github@martinrist.com>
2017-09-04 19:05:24 +0100

    Fix indentation

--- examples/currentTimeServer.js
+++ examples/currentTimeServer.js
@@ -3,7 +3,7 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-   res.writeHead(200, {"Content-Type": "text/plain"});
-   res.write(new Date().toISOString());
-   res.end();
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write(new Date().toISOString());
+    res.end();
 }).listen(9876);
